d17b79823d7c626502e53c78ff2978996180748e
PaddyHly <healyp3@tcd.ie>
2014-02-16 18:55:35 +0000

    league modelled

--- rugbyTest/league/models.py
+++ rugbyTest/league/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
-class League(models.Model):
-    title = models.CharField(max_length=50)
-
-class Fixture(models.Model):
-    home_team = models.ForeignKey(Team)
-    away_team = models.ForeignKey(Team)
-    venue = models.CharField(max_length=50)
-
+#should the teams be completely seperated from modeling the leagues/fixtures,
+#i.e. in a seperate 'app' and then imported in?
+#are we tracking individual players?
 class Team(models.Model):
     name = models.CharField(max_length=50)
-    home_pitch = models.CharField(max_length=50)
+    venue_name = models.CharField(max_length=50)
+    home_pitch_location = models.CharField(max_length=50)
+
+class League(models.Model):
+    title = models.CharField(max_length=50)
+    teams = models.ManyToManyField(Team)
+
+class Fixture(models.Model):
+    league_part_of = models.ForeignKey(League)
+    teams = models.ManyToManyField(Team)
+    venue = models.CharField(max_length=50)
